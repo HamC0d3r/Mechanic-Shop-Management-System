a14a4c71d33a1ddb0a6df50347676a0fe081b22a
hamc0d3r <hamzehmahmood966@gmail.com>
2026-02-07 12:50:00 +0300

    Add Behaviours in application layer

--- src/MechanicShop.Application/Common/Behaviours/CachingBehavior.cs
+++ src/MechanicShop.Application/Common/Behaviours/CachingBehavior.cs
@@ -18,31 +18,39 @@ public class CachingBehavior<TRequest, TResponse>(
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        if(request is not ICachesQuery<TResponse> cacheableRequest)
+        try
         {
-            _logger.LogInformation("Request {RequestType} is not cacheable. Proceeding without caching.", typeof(TRequest).Name);
+            if(request is not ICachesQuery<TResponse> cacheableRequest)
+            {
+                _logger.LogInformation("Request {RequestType} is not cacheable. Proceeding without caching.", typeof(TRequest).Name);
+                return await next(cancellationToken);
+            }
+            _logger.LogInformation("Handling cacheable request {RequestType} with cache key {CacheKey}.", typeof(TRequest).Name, cacheableRequest.CacheKey);
+
+            var result = await _cache.GetOrCreateAsync(key: cacheableRequest.CacheKey, factory: async ct =>
+            {
+                var innerResult = await next(ct);
+                if(innerResult is IResult r && r.IsSuccess)
+                {
+                    return innerResult;
+                }
+                
+                //return default!; // Do not cache errors (failures)
+                throw new SecurityException();
+            },
+            options: new HybridCacheEntryOptions
+            {
+                Expiration = cacheableRequest.Expiration
+            },
+            tags: cacheableRequest.Tags,
+            cancellationToken: cancellationToken);
+
+            return result;
+        }
+        catch(SecurityException)
+        {
             return await next(cancellationToken);
         }
-        _logger.LogInformation("Handling cacheable request {RequestType} with cache key {CacheKey}.", typeof(TRequest).Name, cacheableRequest.CacheKey);
-
-        var result = await _cache.GetOrCreateAsync(key: cacheableRequest.CacheKey, factory: async ct =>
-        {
-            var innerResult = await next(ct);
-            if(innerResult is IResult r && r.IsSuccess)
-            {
-                return innerResult;
-            }
-            
-            return default!; // Do not cache errors (failures)
-        },
-        options: new HybridCacheEntryOptions
-        {
-            Expiration = cacheableRequest.Expiration
-        },
-        tags: cacheableRequest.Tags,
-        cancellationToken: cancellationToken);
-
-        return result;
 
     }
 }
