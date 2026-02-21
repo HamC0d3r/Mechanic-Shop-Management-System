b1ce22450a94ec747d25e5c7974e36f4b3829e20
hamc0d3r <hamzehmahmood966@gmail.com>
2026-02-21 08:43:42 +0300

    add initial migration with SQL server and initiate identity service

--- src/MechanicShop.Infrastructure/Data/Migrations/20260221053635_InitialCreate.cs
+++ src/MechanicShop.Infrastructure/Data/Migrations/20260221053635_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace MechanicShop.Infrastructure.Migrations
+namespace MechanicShop.Infrastructure.Data.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
