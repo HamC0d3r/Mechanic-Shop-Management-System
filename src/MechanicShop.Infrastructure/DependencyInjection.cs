3023e73414dbabf12494b8ba3afbcdce24565ad9
hamc0d3r <hamzehmahmood966@gmail.com>
2026-02-21 11:03:39 +0300

    add Background Job to cancel overdue workorders and modified on dependency injection file

--- src/MechanicShop.Infrastructure/DependencyInjection.cs
+++ src/MechanicShop.Infrastructure/DependencyInjection.cs
@@ -6,12 +6,24 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 using MechanicShop.Infrastructure.Data.Interceptors;
 using MechanicShop.Infrastructure.Data;
 using MechanicShop.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MechanicShop.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using MechanicShop.Infrastructure.Identity.Policies;
+using Microsoft.Extensions.Caching.Hybrid;
+using MechanicShop.Infrastructure.Services;
+using MechanicShop.Infrastructure.RealTime;
+using MechanicShop.Infrastructure.BackgroundJobs;
 namespace MechanicShop.Infrastructure;
 
 public static class DependancyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddSingleton(TimeProvider.System);
+
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         ArgumentNullException.ThrowIfNull(connectionString);
@@ -28,6 +40,74 @@ public static class DependancyInjection
         services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
 
         services.AddScoped<ApplicationDbContextInitialiser>();
+
+        
+        
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(jwtSettings["Secret"]!)),
+            };
+        });
+
+
+           services
+        .AddIdentityCore<AppUser>(options =>
+        {
+            options.Password.RequiredLength = 6;
+            options.Password.RequireDigit = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequiredUniqueChars = 1;
+            options.SignIn.RequireConfirmedAccount = false;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<AppDbContext>();
+
+        services.AddScoped<IAuthorizationHandler, LaborAssignedHandler>();
+
+        services.AddAuthorizationBuilder()
+              .AddPolicy("ManagerOnly", policy => policy.RequireRole("Manager"))
+              .AddPolicy("SelfScopedWorkOrderAccess", policy =>
+                policy.Requirements.Add(new LaborAssignedRequirement()));
+
+        services.AddTransient<IIdentityService, IdentityService>();
+
+
+
+        services.AddHybridCache(options => options.DefaultEntryOptions = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromMinutes(10), // L2, L3
+            LocalCacheExpiration = TimeSpan.FromSeconds(30), // L1
+        });
+
+        services.AddScoped<IWorkOrderPolicy, WorkOrderPolicy>();
+
+        services.AddScoped<ITokenProvider, TokenProvider>();
+
+        services.AddScoped<IInvoicePdfGenerator, InvoicePdfGenerator>();
+
+        services.AddScoped<INotificationService, NotificationService>();
+
+        services.AddScoped<IWorkOrderNotifier, SignalRWorkOrderNotifier>();
+
+        services.AddHostedService<OverdueBookingCleanupService>();
 
         return services;
     }
